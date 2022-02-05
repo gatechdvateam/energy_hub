a68d8001a088954cba18b57e0a1faf7ba9bdcd7e
hasmm <hasmm@outlook.com>
2022-02-06 02:57:38 +0400

    Reuploading the working project template to github

---
+++ webapp/backend/app.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
