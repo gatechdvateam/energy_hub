963d1e94a5bb1e06df5bc0f6f9034d7660ad972d
hasmm <hasmm@outlook.com>
2022-02-06 04:18:26 +0400

    Updating Files

--- webapp/backend/app.py
+++ webapp/backend/app.py
@@ -1,6 +1,11 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for
+from azure_utils import KeyVault, DataLake
+import pandas as pd
+
 app = Flask(__name__)
+
+
 
 
 @app.route('/')
@@ -12,6 +17,14 @@ def index():
 @app.route('/hello', methods=['POST'])
 def hello():
    name = request.form.get('name')
+   try:
+      vault = KeyVault(keyVaultName = "keyvaultdva2022")
+      storage_credential = vault.get_secret(secretName = "storagePrimaryKey")
+      storage = DataLake(account_name = "storageaccountdva", credential = storage_credential)
+      df = storage.read(file_system = "energyhub", directory = "/", file_name = "HassanTest.csv", extension = "csv")
+      print(df)
+   except Exception as e:
+      name = e
 
    if name:
        print('Request for hello page received with name=%s' % name)
