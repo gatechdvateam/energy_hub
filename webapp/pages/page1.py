76caee828ce4b338d314879c49464cec12b2ec6e
hasmm <hasmm@outlook.com>
2022-03-03 00:51:30 +0400

    Created DashApp Layout and Added Bootstrap Support

---
+++ webapp/pages/page1.py
@@ -0,0 +1,19 @@
+from dash import dcc, html, Input, Output, callback
+
+def CreateLayout():
+    layout = html.Div([
+    html.H3('Page 1'),
+    dcc.Dropdown(
+        {f'Page 1 - {i}': f'{i}' for i in ['New York City', 'Montreal', 'Los Angeles']},
+        id='page-1-dropdown'
+    ),
+    html.Div(id='page-1-display-value'),
+    dcc.Link('Go to Page 2', href='/page2')
+    ])
+    return layout
+
+@callback(
+    Output('page-1-display-value', 'children'),
+    Input('page-1-dropdown', 'value'))
+def display_value(value):
+    return f'You have selected {value}'
