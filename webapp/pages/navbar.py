cac66d0b778dc47b065f37b2231c9244b16792ef
hasmm <hasmm@outlook.com>
2022-03-06 04:43:59 +0400

    Started Data Visulization

---
+++ webapp/pages/navbar.py
@@ -0,0 +1,22 @@
+from dash import Dash, dcc, html, Input, Output, callback
+import dash_bootstrap_components as dbc
+
+layout = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Index", href="/")),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("More pages", header=True),
+                dbc.DropdownMenuItem("Page 1", href="/page1"),
+                dbc.DropdownMenuItem("Page 2", href="/page2"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="More",
+        ),
+    ],
+    brand="NavbarSimple",
+    brand_href="/",
+    color="primary",
+    dark=True,
+)
