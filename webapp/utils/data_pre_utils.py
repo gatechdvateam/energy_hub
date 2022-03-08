5a776bc3000c49e79cb7c809687b308c1e341b9d
Mai <amienguyen30493@gmail.com>
2022-03-07 21:09:31 -0500

    added function to reduce memory

--- webapp/utils/data_pre_utils.py
+++ webapp/utils/data_pre_utils.py
@@ -1,6 +1,45 @@
+import numpy as np
 import pandas as pd
+import math
+import datetime
+from glob import glob
+import gc
+
 
 def df_val_map(df: pd.DataFrame, col: str, mapping:  dict) -> None:
     """For a given column, map values from one value to the other,
      defined in a mapping dict"""
     df[col] = df[col].map(mapping)
+
+
+# Function to reduce the DF size
+# Source code for this function: https://www.kaggle.com/caesarlupum/ashrae-start-here-a-gentle-introduction
+def reduce_mem_usage(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose:
+        print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(
+            end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
