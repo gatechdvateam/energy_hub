2c4724ae8da61bc1f27f74f6731314664f2afdb3
Siham <siham.elmali@gatech.edu>
2022-03-12 12:32:15 -0800

    added navbar content and slider

--- webapp/pages/home.py
+++ webapp/pages/home.py
@@ -1,6 +1,28 @@
+# import packages
 from dash import Dash, dcc, html, Input, Output, callback
+import dash_bootstrap_components as dbc
 
 
-
-def createLayout():
-    return 'Go back'
+carousel = dbc.Carousel(
+    items=[
+        {
+            "key": "1",
+            "src": "/assets/images/background_image_resized2.jpeg",
+            "header": "Welcome to Energy Hub"
+            # "caption": "and caption",
+        },
+        {
+            "key": "2",
+            "src": "/assets/images/background_image_resized2.jpeg",
+            # "header": "With header only",
+            # "caption": "",
+        },
+        {
+            "key": "3",
+            "src": "/assets/images/background_image_resized2.jpeg",
+            # "header": "",
+            # "caption": "This slide has a caption only",
+        },
+    ],
+    variant="light",
+)
