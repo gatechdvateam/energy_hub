c02d9c16ef1668959904adce447259e77b02f815
Siham <siham.elmali@gatech.edu>
2022-03-24 19:07:01 -0700

    cleaned up data.py and dataoverview.py

--- webapp/data.py
+++ webapp/data.py
@@ -53,7 +53,7 @@ def get_data(path, filename):
     return dataset
 
 
-def get_buidling_by_primary_usage(metadata, selected_site):
+def get_buidling_by_space_usage(metadata, space_usage, selected_site):
     """_takes metadata and a selected site, then return dadaset for sites,
         count of building it has and spage usage_
 
@@ -66,7 +66,7 @@ def get_buidling_by_primary_usage(metadata, selected_site):
     """
 
     # get datafarme
-    buildings = metadata[['site_id','building_id', 'primary_space_usage']]
+    buildings = metadata[['site_id','building_id', space_usage]]
 
 
     if (selected_site != None) and (len(selected_site)!=0):
@@ -75,62 +75,23 @@ def get_buidling_by_primary_usage(metadata, selected_site):
         buildings = buildings.loc[buildings['site_id'].isin(selected_site)]
 
     # filter buildings that have primary usage listed
-    buildings = buildings[buildings['primary_space_usage'].notnull()]
+    buildings = buildings[buildings[space_usage].notnull()]
 
     # Aggregate
-    buildings = buildings.groupby(['site_id', 'primary_space_usage'], as_index=False).count()
+    buildings = buildings.groupby(['site_id', space_usage], as_index=False).count()
 
     # rename the columns
     buildings = buildings.rename(columns={'site_id': 'Sites',
                                         'building_id': 'Number of Buildings',
-                                        'primary_space_usage': 'Space Usage'})
+                                        space_usage: 'Space Usage'})
     buildings = buildings.sort_values(by=['Number of Buildings', 'Space Usage'],ascending=False)
 
-    # get builings that have more than 15 sub buildings
-    buildings = buildings[buildings['Number of Buildings'] > 15]
-
-
-    return buildings
-
-
-def get_buidling_by_secondary_usage(metadata, selected_site):
-    """_takes metadata and a selcted site, then return dadaset for sites,
-        count of building it has and spage usage_
-
-    Args:
-        metadata (_dataFrame_): _description_
-        selected_site (_string_): _description_
-
-    Returns:
-        _buildings_: _dataFrame_
-    """
-
-    # get datafarme
-    buildings = metadata[['site_id','building_id', 'sub_primary_space_usage']]
-
-
-    if (selected_site != None) and (len(selected_site)!=0):
-        
-        # get builidings from the selected sites
-        buildings = buildings.loc[buildings['site_id'].isin(selected_site)]
-
-    # filter buildings that have primary usage listed
-    buildings = buildings[buildings['sub_primary_space_usage'].notnull()]
-
-    # Aggregate
-    buildings = buildings.groupby(['site_id', 'sub_primary_space_usage'], as_index=False).count()
-
-    # rename the columns
-    buildings = buildings.rename(columns={'site_id': 'Sites',
-                                        'building_id': 'Number of Buildings',
-                                        'sub_primary_space_usage': 'Space Usage'})
-    buildings = buildings.sort_values(by=['Number of Buildings', 'Space Usage'],ascending=False)
-
-    # get builings that have at least 10 sub buildings
+    # get builings that have 10  or more buildings
     buildings = buildings[buildings['Number of Buildings'] >= 10]
 
 
     return buildings
+
 
 def get_meter_data_for_building(MeterName,BuildingName):
     """
@@ -140,5 +101,7 @@ def get_meter_data_for_building(MeterName,BuildingName):
     meterdata = get_dask_data(location, "*.parquet")
     return meterdata
 
+
 # Preload Small Datasets here.
 BuildingMetadata = get_data("/data_parq/metadata/", "metadata.parq").reset_index().copy()
+weatherDate = get_data("/data_parq/weather/", "weather.parq").reset_index().copy()
